69e88de538d5b542265608fb204588a0fa90285c
Isak <isak.drevstad10@gmail.com>
2024-02-18 23:34:45 +0100

    refactor: Use `src/` directory

--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-    return <h1 className="text-green-600">Hello World</h1>;
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={`${inter.className} bg-zinc-800 text-white`}>
+                {children}
+            </body>
         </html>
     );
 }
