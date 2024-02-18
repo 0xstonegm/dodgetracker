4720727b5fa887506e5f25308f144d7b9669c41d
Isak <isak.drevstad10@gmail.com>
2024-02-18 23:40:10 +0100

    feat(update_riotapi_key): Ask for confirmation before continuing

--- scripts/update_riotapi_key.sh
+++ scripts/update_riotapi_key.sh
@@ -2,19 +2,29 @@
 set -e
 
 # Script to update the RiotAPI key in github secrets and the Lambda function configuration
-
 api_key=$1
 [ "$api_key" = "" ] && echo "Usage: $0 <api_key>" && exit 1
 
-ENV_VARS_FILE="../.env.txt"
-[ ! -f "$ENV_VARS_FILE" ] && echo "File not found: $(realpath "$ENV_VARS_FILE")" && exit 1
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-sed -i "s/^RIOT_API_KEY=.*/RIOT_API_KEY=$api_key/" "$ENV_VARS_FILE"
+env_vars_file="$(realpath "$SCRIPT_DIR/../.env.txt")"
+[ ! -f "$env_vars_file" ] && echo "File not found: $(realpath "$env_vars_file")" && exit 1
 
-LAMBDA_FUNCTION_NAME="dodgetracker-UpdatePlayersAndDodges-v0GzQQD5rILI"
+sed -i "s/^RIOT_API_KEY=.*/RIOT_API_KEY=$api_key/" "$env_vars_file"
+
+# Ask for confirmation that you want to deploy the new API key
+read -p "Are you sure you want to deploy the new API key? [y/N] " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo -e "\nContinuing..."
+else
+    echo "API key deployment cancelled."
+    exit 0
+fi
+
+LAMBDA_FUNCTION_NAME="dodgetracker-UpdatePlayersAndDodges-v0GzQQD5rILI" # FIXME: break out into .env file
 
 # Construct the environment variables string in the required format
-ENV_VARS_STRING=$(awk -F'=' '{print $1"="$2}' "$ENV_VARS_FILE" | paste -sd ',' -)
+ENV_VARS_STRING=$(awk -F'=' '{print $1"="$2}' "$env_vars_file" | paste -sd ',' -)
 
 # Update the Lambda function configuration with the new environment variables
 aws lambda update-function-configuration --function-name "$LAMBDA_FUNCTION_NAME" --environment "Variables={$ENV_VARS_STRING}"
