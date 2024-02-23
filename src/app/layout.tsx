4e4041a0513f5f92359af5024476064db06af2a5
Isak <isak.drevstad10@gmail.com>
2024-02-23 16:54:09 +0100

    feat: Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "../components/NavBar";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
             <body className={`${inter.className} bg-zinc-700 text-zinc-300`}>
                 <NavBar />
                 {children}
+                <Analytics />
             </body>
         </html>
     );
