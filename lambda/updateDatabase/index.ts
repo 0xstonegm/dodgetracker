10c61290f4559f66931775755bbe9d5fdbeb97a5
Isak <isak.drevstad10@gmail.com>
2024-02-28 20:04:56 +0100

    fix(backend): Rollback if timeout exceeded to avoid DB lock

--- lambda/updateDatabase/index.ts
+++ lambda/updateDatabase/index.ts
@@ -37,62 +37,53 @@ async function checkAccountsAndSummonersCount(connection: PoolConnection) {
     }
 }
 
-export async function run() {
-    let connection: PoolConnection | null = null;
-    try {
-        connection = await pool.getConnection();
-        await connection.beginTransaction();
+export async function run(connection: PoolConnection) {
+    let newData = await getPlayers();
+    let oldData = await fetchCurrentPlayers(connection);
 
-        let newData = await getPlayers();
-        let oldData = await fetchCurrentPlayers(connection);
+    logger.info(
+        `Fetched ${newData.size} players from API and ${oldData.size} from DB. (diff = ${oldData.size - newData.size})`,
+    );
 
-        logger.info(
-            `Fetched ${newData.size} players from API and ${oldData.size} from DB. (diff = ${oldData.size - newData.size})`,
-        );
+    let dodges = await getDodges(oldData, newData);
 
-        let dodges = await getDodges(oldData, newData);
-
-        if (dodges.length > 0) {
-            await updateAccountsData(dodges, connection);
-            await insertDodges(dodges, connection);
-        }
-        if (newData.size > 0) {
-            await upsertPlayers(newData, connection);
-        }
-
-        await registerPromotions(oldData, newData, connection);
-        await registerDemotions(oldData, newData, connection);
-
-        await connection.commit();
-        logger.info("Transaction successfully committed!");
-
-        await checkAccountsAndSummonersCount(connection);
-    } catch (error) {
-        if (connection) {
-            await connection.rollback();
-        }
-        logger.error("Transaction failed and rolled back", error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: "Error processing request",
-                error,
-            }),
-        };
-    } finally {
-        if (connection) {
-            connection.release();
-        }
+    if (dodges.length > 0) {
+        await updateAccountsData(dodges, connection);
+        await insertDodges(dodges, connection);
+    }
+    if (newData.size > 0) {
+        await upsertPlayers(newData, connection);
     }
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "Database updated successfully.",
-        }),
-    };
+    await registerPromotions(oldData, newData, connection);
+    await registerDemotions(oldData, newData, connection);
+
+    await checkAccountsAndSummonersCount(connection);
 }
 
 export const handler: Handler = async () => {
-    return run();
+    const connection = await pool.getConnection();
+    try {
+        await connection.beginTransaction();
+        logger.info("Transaction started...");
+
+        await run(connection);
+
+        await connection.commit();
+        logger.info("Database updated successfully, transaction committed.");
+        connection.release();
+
+        return {
+            statusCode: 200,
+            body: "Database updated successfully",
+        };
+    } catch (error) {
+        await connection.rollback();
+        logger.error("Database update failed, transaction rolled back:", error);
+
+        return {
+            statusCode: 500,
+            body: "Database update failed",
+        };
+    }
 };
--- lambda/updateDatabase/main.ts
+++ lambda/updateDatabase/main.ts
@@ -1,3 +1,4 @@
+import pool from "./db";
 import { run } from "./index";
 import logger from "./logger";
 
@@ -8,18 +9,30 @@ function timeout(ms: number) {
 }
 
 async function main() {
+    const connection = await pool.getConnection();
+
     while (true) {
         // START TIMING
         const startTime = new Date();
-
         logger.info(
             "----------------------------RUNNING DATABASE UPDATE----------------------------",
         );
+
+        await connection.beginTransaction();
+        logger.info("Transaction started...");
+
         try {
-            await Promise.race([run(), timeout(30 * 1000)]);
-            logger.info("Database updated successfully");
+            await Promise.race([run(connection), timeout(30 * 1000)]);
+            connection.commit();
+            logger.info(
+                "Database updated successfully, transaction committed.",
+            );
         } catch (error) {
-            logger.error("Database update failed", error);
+            logger.error(
+                "Database update failed, transaction rolled back:",
+                error,
+            );
+            connection.rollback();
         }
 
         if (global.gc) {
@@ -30,6 +43,8 @@ async function main() {
         const endTime = new Date();
         const timeDiff = endTime.getTime() - startTime.getTime();
         logger.info(`PERFORMANCE: Database update took ${timeDiff} ms\n`);
+
+        // TODO: handle SIGINT and SIGTERM
     }
 }
 
