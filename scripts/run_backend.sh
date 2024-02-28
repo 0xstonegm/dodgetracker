b27bc0ca660d4f61d55bf1b1241f6e4e80830432
Isak <isak.drevstad10@gmail.com>
2024-02-28 10:51:39 +0100

    fix(run_backend.sh): Fix node path on HP

--- scripts/run_backend.sh
+++ scripts/run_backend.sh
@@ -2,18 +2,23 @@
 set -e
 
 PROJECT_ROOT_DIR=$(realpath "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)/../")
-NODE_BIN="/home/isak102/.nvm/versions/node/v21.6.2/bin/node"
+
+if [ "$HOSTNAME" = "HP" ]; then
+    NODE_BIN="/usr/bin/node"
+else
+    NODE_BIN="/home/isak102/.nvm/versions/node/v21.6.2/bin/node"
+fi
 
 cd "$PROJECT_ROOT_DIR"
 
 source .env
 
 echo "Building backend..."
-$NODE_BIN "build-backend.js"
+"$NODE_BIN" "build-backend.js"
 echo "Build completed."
 
 echo "Starting backend..."
-$NODE_BIN "lambda/updateDatabase/dist/main.js"
+"$NODE_BIN" "lambda/updateDatabase/dist/main.js"
 echo "Backend finished running."
 
 cd -
