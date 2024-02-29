800e0ae6950d1230866e78c10e9cc2f97135aa6c
Isak <isak.drevstad10@gmail.com>
2024-02-29 15:46:30 +0100

    fix(metadata): Set `metadataBase`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+    metadataBase: new URL("https://www.dodgetracker.com"),
     title: {
         default: "Dodgetracker",
         template: "%s - Dodgetracker",
