23cd11c2052b5399bb1ced52824428754dec3587
Isak <isak.drevstad10@gmail.com>
2024-05-03 17:54:31 +0200

    feat: Add Stats page route

---
+++ src/app/stats/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Stats",
+    description: "Dodge statistics",
+};
+
+export default function Stats() {
+    return (
+        <div className="flex h-[90vh] items-center justify-center">
+            <p>Dodge statistics will be available soon.</p>
+        </div>
+    );
+}
