b6990f7dcf5c46b76d81f340cc6e054d90f7f220
Isak <isak.drevstad10@gmail.com>
2024-05-05 00:10:23 +0200

    feat(metadata): List available regions in description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,14 @@ import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "../components/NavBar";
+import { supportedUserRegions } from "../regions";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const availableRegions = [...supportedUserRegions]
+    .map((r) => r.toUpperCase())
+    .join(", ");
 
 export const metadata: Metadata = {
     metadataBase: new URL("https://www.dodgetracker.com"),
@@ -13,7 +18,7 @@ export const metadata: Metadata = {
         default: "Dodgetracker - League of Legends",
         template: "%s - Dodgetracker - League of Legends",
     },
-    description: "Track League of Legends dodges in master+",
+    description: `Track League of Legends dodges in Master, Grandmaster, and Challenger. Available for ${availableRegions}.`,
     twitter: {
         card: "summary_large_image",
     },
