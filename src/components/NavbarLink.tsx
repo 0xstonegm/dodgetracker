d138b6a3b5cf1fd981215a33312699bc25ab8ac8
Isak <isak.drevstad10@gmail.com>
2024-05-04 20:16:07 +0200

    fix: Fix About -> Leaderboard navigation

--- src/components/NavbarLink.tsx
+++ src/components/NavbarLink.tsx
@@ -2,19 +2,34 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { supportedUserRegions } from "../regions";
 
 export default function NavbarLink({ path }: { path: string }) {
-    let currentUserRegion = usePathname().split("/")[1];
+    let currentUserRegion: string | null = usePathname().split("/")[1];
     currentUserRegion = (function () {
-        // TODO: redirect to correct region
-        if (currentUserRegion === "about") {
-            return "";
+        if (!supportedUserRegions.has(currentUserRegion)) {
+            return null;
         }
         return currentUserRegion;
     })();
 
+    function getNewPath(destination: string) {
+        let path = destination.toLowerCase();
+
+        if (!currentUserRegion) {
+            if (path === "leaderboard") {
+                // FIXME: Temporary fix, should be replaced with a proper solution
+                return "/euw/leaderboard";
+            } else {
+                return `/${path}`;
+            }
+        } else {
+            return `/${currentUserRegion}/${path}`;
+        }
+    }
+
     return (
-        <Link href={`/${currentUserRegion}/${path.toLowerCase()}`}>
+        <Link href={getNewPath(path)}>
             <div className="text-lg md:text-xl">{path}</div>
         </Link>
     );
