dcc6730472b29aabf2dac05fe58a257565df73af
Isak <isak.drevstad10@gmail.com>
2024-05-13 17:06:44 +0200

    chore: Remove backend build files

--- src/backend/dist/src/backend/api.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.riotApi = exports.lolApi = void 0;
-const twisted_1 = require("twisted");
-exports.lolApi = new twisted_1.LolApi();
-exports.riotApi = new twisted_1.RiotApi();
--- src/backend/dist/src/backend/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Tier = void 0;
-var Tier;
-(function (Tier) {
-    Tier["MASTER"] = "MASTER";
-    Tier["GRANDMASTER"] = "GRANDMASTER";
-    Tier["CHALLENGER"] = "CHALLENGER";
-})(Tier || (exports.Tier = Tier = {}));
