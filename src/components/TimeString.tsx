cd5e464c172788eeab33c9f67192f15714c3fd5a
Isak <isak.drevstad10@gmail.com>
2024-05-15 09:16:45 +0200

    refactor: Move time string function to utils

--- src/components/TimeString.tsx
+++ src/components/TimeString.tsx
@@ -1,34 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
-function timeToString(utcTime: Date): string {
-  const timeDiff = Date.now() - utcTime.getTime();
-
-  const diffInSeconds: number = timeDiff / 1000;
-  const diffInMinutes: number = diffInSeconds / 60;
-  const diffInHours: number = diffInMinutes / 60;
-  const diffInDays: number = diffInHours / 24;
-
-  if (diffInDays > 1) {
-    const remainingHours = Math.floor(diffInHours % 24);
-    return remainingHours === 0
-      ? `${Math.floor(diffInDays)}d ago`
-      : `${Math.floor(diffInDays)}d ${Math.floor(remainingHours)}h ago`;
-  } else if (diffInHours > 1) {
-    const remainingMinutes = Math.floor(diffInMinutes % 60);
-    return remainingMinutes === 0
-      ? `${Math.floor(diffInHours)}h ago`
-      : `${Math.floor(diffInHours)}h ${remainingMinutes}min ago`;
-  } else if (diffInMinutes > 1) {
-    const remainingSeconds = diffInSeconds % 60;
-    return diffInMinutes < 5
-      ? `${Math.floor(diffInMinutes)}min ${Math.floor(remainingSeconds)}s ago`
-      : `${Math.floor(diffInMinutes)}min ago`;
-  } else {
-    return `${Math.floor(diffInSeconds)}s ago`;
-  }
-}
+import { timeDiffString } from "../lib/utils";
 
 export default function TimeString({
   utcTime: utcTime,
@@ -53,7 +26,7 @@ export default function TimeString({
 
   return (
     <p title={`Dodge detected at:\n${utcTime.toLocaleString()}`}>
-      {timeToString(utcTime)}
+      {timeDiffString(utcTime)}
     </p>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,31 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function timeDiffString(utcTime: Date): string {
+  const timeDiff = Date.now() - utcTime.getTime();
+
+  const diffInSeconds: number = timeDiff / 1000;
+  const diffInMinutes: number = diffInSeconds / 60;
+  const diffInHours: number = diffInMinutes / 60;
+  const diffInDays: number = diffInHours / 24;
+
+  if (diffInDays > 1) {
+    const remainingHours = Math.floor(diffInHours % 24);
+    return remainingHours === 0
+      ? `${Math.floor(diffInDays)}d ago`
+      : `${Math.floor(diffInDays)}d ${Math.floor(remainingHours)}h ago`;
+  } else if (diffInHours > 1) {
+    const remainingMinutes = Math.floor(diffInMinutes % 60);
+    return remainingMinutes === 0
+      ? `${Math.floor(diffInHours)}h ago`
+      : `${Math.floor(diffInHours)}h ${remainingMinutes}min ago`;
+  } else if (diffInMinutes > 1) {
+    const remainingSeconds = diffInSeconds % 60;
+    return diffInMinutes < 5
+      ? `${Math.floor(diffInMinutes)}min ${Math.floor(remainingSeconds)}s ago`
+      : `${Math.floor(diffInMinutes)}min ago`;
+  } else {
+    return `${Math.floor(diffInSeconds)}s ago`;
+  }
+}
