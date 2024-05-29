ac428f38c16b9244495558b78fbf1b376217a6d2
Isak <isak.drevstad10@gmail.com>
2024-05-29 11:09:17 +0200

    chore: Install `react-query` devtools

--- src/components/higherOrder/queryClientProvider.tsx
+++ src/components/higherOrder/queryClientProvider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 
 const queryClient = new QueryClient();
@@ -10,6 +11,9 @@ export const ReactQueryClientProvider = ({
   children: React.ReactNode;
 }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
