1424838485292c85496fc5cbb66f75ecd62dda75
Isak <isak.drevstad10@gmail.com>
2024-05-29 12:37:19 +0200

    feat(SearchBar): Add scrolling

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -76,97 +76,102 @@ export default function SearchBar({ className }: SearchBarProps) {
   return (
     <>
       <div className="relative">
-        <Input
-          className={cn("w-96", className)}
-          placeholder="Search players..."
-          value={searchFilter}
-          onChange={inputHandleChange}
-          onClick={() => setInputHasFocus(true)}
-          onFocus={() => {
-            setInputHasFocus(true);
-            posthog.capture("searchbar_focused");
-          }}
-          ref={ref as any}
-        />
-        <div className="absolute top-10 z-[100] flex w-full flex-col justify-center rounded-sm bg-zinc-900 shadow-sm shadow-zinc-900">
-          {inputHasFocus && data && data.players.length > 0 && (
-            <ul>
-              {data.players.map((player) => (
-                <li
-                  className="hover:bg-zinc-800"
-                  key={`${player.gameName}#${player.tagLine}`}
-                >
-                  <Link
-                    className="flex items-center border-b border-zinc-800 p-1"
-                    href={`/${region}/${player.gameName}-${player.tagLine}`}
-                    onClick={() => {
-                      setSearchFilter("");
-                      posthog.capture("searched_player", {
-                        region: region,
-                        gameName: player.gameName,
-                        tagLine: player.tagLine,
-                      });
-                    }}
-                  >
-                    <div className="flex items-center">
-                      <Image
-                        className="pr-1"
-                        alt="Profile Icon"
-                        src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${player.profileIconId}.jpg`}
-                        width={32}
-                        height={32}
-                        unoptimized
-                      />
-                      <div className="flex flex-col">
-                        <p className="font-semibold">
-                          {player.gameName}#{player.tagLine}
-                        </p>
-                        <div className="text-xs font-light">
-                          <div className="flex items-center gap-1">
-                            {isWithinDays(
-                              player.lastUpdatedAt,
-                              new Date(),
-                              3,
-                            ) && (
-                              <>
-                                <Image
-                                  src={getRankEmblem(player.rankTier as Tier)}
-                                  alt={player.rankTier}
-                                  quality={100}
-                                  width={16}
-                                  height={16}
-                                  unoptimized
-                                />
-                                <p>{player.lp} LP</p>
-                                <Dot size={10} />
-                              </>
-                            )}
-                            <p>LVL {player.summonerLevel}</p>
+        <div ref={ref as any}>
+          <Input
+            className={cn("w-96", className)}
+            placeholder="Search players..."
+            value={searchFilter}
+            onChange={inputHandleChange}
+            onClick={() => setInputHasFocus(true)}
+            onFocus={() => {
+              setInputHasFocus(true);
+              posthog.capture("searchbar_focused");
+            }}
+          />
+          {inputHasFocus && searchFilter.length > 0 && (
+            <div className="absolute top-10 z-[100] flex h-[50vh] w-full flex-col overflow-y-scroll rounded-sm bg-zinc-900 shadow-sm shadow-zinc-900 scrollbar scrollbar-track-zinc-800 scrollbar-thumb-zinc-900">
+              {inputHasFocus && data && data.players.length > 0 && (
+                <ul>
+                  {data.players.map((player) => (
+                    <li
+                      className="hover:bg-zinc-800"
+                      key={`${player.gameName}#${player.tagLine}`}
+                    >
+                      <Link
+                        className="flex items-center border-b border-zinc-800 p-1"
+                        href={`/${region}/${player.gameName}-${player.tagLine}`}
+                        onClick={() => {
+                          setSearchFilter("");
+                          posthog.capture("searched_player", {
+                            region: region,
+                            gameName: player.gameName,
+                            tagLine: player.tagLine,
+                          });
+                        }}
+                      >
+                        <div className="flex items-center">
+                          <Image
+                            className="pr-1"
+                            alt="Profile Icon"
+                            src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${player.profileIconId}.jpg`}
+                            width={32}
+                            height={32}
+                            unoptimized
+                          />
+                          <div className="flex flex-col">
+                            <p className="font-semibold">
+                              {player.gameName}#{player.tagLine}
+                            </p>
+                            <div className="text-xs font-light">
+                              <div className="flex items-center gap-1">
+                                {isWithinDays(
+                                  player.lastUpdatedAt,
+                                  new Date(),
+                                  3,
+                                ) && (
+                                  <>
+                                    <Image
+                                      src={getRankEmblem(
+                                        player.rankTier as Tier,
+                                      )}
+                                      alt={player.rankTier}
+                                      quality={100}
+                                      width={16}
+                                      height={16}
+                                      unoptimized
+                                    />
+                                    <p>{player.lp} LP</p>
+                                    <Dot size={10} />
+                                  </>
+                                )}
+                                <p>LVL {player.summonerLevel}</p>
+                              </div>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </div>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          )}
-          {inputHasFocus && isPending && searchFilter.length > 0 && (
-            <div className="flex items-center justify-center p-2">
-              <p>Loading...</p>
-            </div>
-          )}
-          {inputHasFocus &&
-            data &&
-            searchFilter.length > 0 &&
-            data.players.length === 0 && (
-              <div className="flex items-center justify-center p-2">
-                <p>No players found.</p>
-              </div>
-            )}
-          {inputHasFocus && error && searchFilter.length > 0 && (
-            <div className="flex items-center justify-center p-2">
-              <p>Search error: {error.message}</p>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
+              {inputHasFocus && isPending && searchFilter.length > 0 && (
+                <div className="flex items-center justify-center p-2">
+                  <p>Loading...</p>
+                </div>
+              )}
+              {inputHasFocus &&
+                data &&
+                searchFilter.length > 0 &&
+                data.players.length === 0 && (
+                  <div className="flex items-center justify-center p-2">
+                    <p>No players found.</p>
+                  </div>
+                )}
+              {inputHasFocus && error && searchFilter.length > 0 && (
+                <div className="flex items-center justify-center p-2">
+                  <p>Search error: {error.message}</p>
+                </div>
+              )}
             </div>
           )}
         </div>
