cc7f7d602b27fe22f834aead0b34eeb329bdd35a
Isak <isak.drevstad10@gmail.com>
2024-05-30 16:35:29 +0200

    feat(SearchBar): Open first entry when pressing Enter

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { Kbd } from "@nextui-org/kbd";
 import { useQuery } from "@tanstack/react-query";
 import { useClickAway, useDebounce } from "@uidotdev/usehooks";
 import { Dot } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import posthog from "posthog-js";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { z } from "zod";
 import { cn, getRankEmblem } from "../lib/utils";
 import { userRegionToRiotRegion } from "../regions";
@@ -54,12 +55,16 @@ export default function SearchBar({ className }: SearchBarProps) {
   const [inputHasFocus, setInputHasFocus] = useState(false);
   const [searchFilter, setSearchFilter] = useState("");
   const debouncedSearchFilter = useDebounce(searchFilter, 250);
+
   const region = usePathname().split("/")[1];
+  const router = useRouter();
 
   const ref = useClickAway(async () => {
     await new Promise((resolve) => setTimeout(resolve, 100));
     setInputHasFocus(false);
   });
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const { data, isPending, error } = useQuery({
     queryKey: ["players", region, debouncedSearchFilter.toLowerCase()],
@@ -73,12 +78,36 @@ export default function SearchBar({ className }: SearchBarProps) {
     setInputHasFocus(true);
   };
 
+  const captureSearch = (gameName: string, tagLine: string) => {
+    posthog.capture("searched_player", {
+      region: region,
+      gameName: gameName,
+      tagLine: tagLine,
+    });
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== "Enter") return;
+    if (isPending || error || !data || data.players.length === 0) return;
+
+    const player = data.players[0];
+    const url = `/${region}/${player.gameName}-${player.tagLine}`;
+    router.push(url);
+
+    setSearchFilter("");
+    inputRef.current?.blur();
+
+    captureSearch(player.gameName, player.tagLine);
+    posthog.capture("searchbar_enter_search");
+  };
+
   return (
     <>
       <div className="relative">
         <div ref={ref as any}>
           <Input
             className={cn("w-96", className)}
+            ref={inputRef}
             placeholder="Search players..."
             disabled={region === "about"} // FIXME: Temporary fix
             value={searchFilter}
@@ -88,12 +117,13 @@ export default function SearchBar({ className }: SearchBarProps) {
               setInputHasFocus(true);
               posthog.capture("searchbar_focused");
             }}
+            onKeyDown={handleKeyDown}
           />
           {inputHasFocus && searchFilter.length > 0 && (
-            <div className="absolute top-10 z-[100] flex h-[50vh] w-full flex-col overflow-y-scroll rounded-sm bg-zinc-900 shadow-sm shadow-zinc-900 scrollbar scrollbar-track-zinc-800 scrollbar-thumb-zinc-900">
+            <div className="absolute top-10 z-[100] flex h-[50vh] w-full flex-col overflow-y-scroll rounded-sm bg-zinc-900 text-zinc-300 shadow-sm shadow-zinc-900 scrollbar scrollbar-track-zinc-800 scrollbar-thumb-zinc-900">
               {inputHasFocus && data && data.players.length > 0 && (
                 <ul>
-                  {data.players.map((player) => (
+                  {data.players.map((player, index) => (
                     <li
                       className="hover:bg-zinc-800"
                       key={`${player.gameName}#${player.tagLine}`}
@@ -103,52 +133,58 @@ export default function SearchBar({ className }: SearchBarProps) {
                         href={`/${region}/${player.gameName}-${player.tagLine}`}
                         onClick={() => {
                           setSearchFilter("");
-                          posthog.capture("searched_player", {
-                            region: region,
-                            gameName: player.gameName,
-                            tagLine: player.tagLine,
-                          });
+                          captureSearch(player.gameName, player.tagLine);
                         }}
                       >
-                        <div className="flex items-center">
-                          <Image
-                            className="pr-1"
-                            alt="Profile Icon"
-                            src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${player.profileIconId}.jpg`}
-                            width={32}
-                            height={32}
-                            unoptimized
-                          />
-                          <div className="flex flex-col">
-                            <p className="font-semibold">
-                              {player.gameName}#{player.tagLine}
-                            </p>
-                            <div className="text-xs font-light">
-                              <div className="flex items-center gap-1">
-                                {isWithinDays(
-                                  player.lastUpdatedAt,
-                                  new Date(),
-                                  3,
-                                ) && (
-                                  <>
-                                    <Image
-                                      src={getRankEmblem(
-                                        player.rankTier as Tier,
-                                      )}
-                                      alt={player.rankTier}
-                                      quality={100}
-                                      width={16}
-                                      height={16}
-                                      unoptimized
-                                    />
-                                    <p>{player.lp} LP</p>
-                                    <Dot size={10} />
-                                  </>
-                                )}
-                                <p>LVL {player.summonerLevel}</p>
+                        <div className="flex w-full items-center justify-between">
+                          <div className="flex items-center">
+                            <Image
+                              className="pr-1"
+                              alt="Profile Icon"
+                              src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${player.profileIconId}.jpg`}
+                              width={32}
+                              height={32}
+                              unoptimized
+                            />
+                            <div className="flex flex-col">
+                              <p className="font-semibold">
+                                {player.gameName}#{player.tagLine}
+                              </p>
+                              <div className="text-xs font-light">
+                                <div className="flex items-center gap-1">
+                                  {isWithinDays(
+                                    player.lastUpdatedAt,
+                                    new Date(),
+                                    3,
+                                  ) && (
+                                    <>
+                                      <Image
+                                        src={getRankEmblem(
+                                          player.rankTier as Tier,
+                                        )}
+                                        alt={player.rankTier}
+                                        quality={100}
+                                        width={16}
+                                        height={16}
+                                        unoptimized
+                                      />
+                                      <p>{player.lp} LP</p>
+                                      <Dot size={10} />
+                                    </>
+                                  )}
+                                  <p>LVL {player.summonerLevel}</p>
+                                </div>
                               </div>
                             </div>
                           </div>
+                          {index == 0 && (
+                            <Kbd
+                              keys={"enter"}
+                              className="mr-1 rounded-md border border-zinc-300 p-1 text-center text-xs text-zinc-300"
+                            >
+                              Enter
+                            </Kbd>
+                          )}
                         </div>
                       </Link>
                     </li>
