f0e42ed4c1e5beba0cf02e9dbc7993e80843db10
Isak <isak.drevstad10@gmail.com>
2024-05-30 16:49:15 +0200

    refactor(SearchBar): Make conditional logic simpler, rename `ref`

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -59,11 +59,10 @@ export default function SearchBar({ className }: SearchBarProps) {
   const region = usePathname().split("/")[1];
   const router = useRouter();
 
-  const ref = useClickAway(async () => {
+  const wrapperRef = useClickAway<HTMLDivElement>(async () => {
     await new Promise((resolve) => setTimeout(resolve, 100));
     setInputHasFocus(false);
   });
-
   const inputRef = useRef<HTMLInputElement>(null);
 
   const { data, isPending, error } = useQuery({
@@ -104,7 +103,7 @@ export default function SearchBar({ className }: SearchBarProps) {
   return (
     <>
       <div className="relative">
-        <div ref={ref as any}>
+        <div ref={wrapperRef}>
           <Input
             className={cn("w-96", className)}
             ref={inputRef}
@@ -121,95 +120,90 @@ export default function SearchBar({ className }: SearchBarProps) {
           />
           {inputHasFocus && searchFilter.length > 0 && (
             <div className="absolute top-10 z-[100] flex h-[50vh] w-full flex-col overflow-y-scroll rounded-sm bg-zinc-900 text-zinc-300 shadow-sm shadow-zinc-900 scrollbar scrollbar-track-zinc-800 scrollbar-thumb-zinc-900">
-              {inputHasFocus && data && data.players.length > 0 && (
+              {data && data.players.length > 0 && (
                 <ul>
                   {data.players.map((player, index) => (
                     <li
-                      className="hover:bg-zinc-800"
+                      className="flex w-full items-center justify-between px-1 hover:bg-zinc-800"
                       key={`${player.gameName}#${player.tagLine}`}
                     >
                       <Link
-                        className="flex items-center border-b border-zinc-800 p-1"
+                        className="flex w-full items-center justify-between border-b border-zinc-800 p-1"
                         href={`/${region}/${player.gameName}-${player.tagLine}`}
                         onClick={() => {
                           setSearchFilter("");
                           captureSearch(player.gameName, player.tagLine);
                         }}
                       >
-                        <div className="flex w-full items-center justify-between">
-                          <div className="flex items-center">
-                            <Image
-                              className="pr-1"
-                              alt="Profile Icon"
-                              src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${player.profileIconId}.jpg`}
-                              width={32}
-                              height={32}
-                              unoptimized
-                            />
-                            <div className="flex flex-col">
-                              <p className="font-semibold">
-                                {player.gameName}#{player.tagLine}
-                              </p>
-                              <div className="text-xs font-light">
-                                <div className="flex items-center gap-1">
-                                  {isWithinDays(
-                                    player.lastUpdatedAt,
-                                    new Date(),
-                                    3,
-                                  ) && (
-                                    <>
-                                      <Image
-                                        src={getRankEmblem(
-                                          player.rankTier as Tier,
-                                        )}
-                                        alt={player.rankTier}
-                                        quality={100}
-                                        width={16}
-                                        height={16}
-                                        unoptimized
-                                      />
-                                      <p>{player.lp} LP</p>
-                                      <Dot size={10} />
-                                    </>
-                                  )}
-                                  <p>LVL {player.summonerLevel}</p>
-                                </div>
+                        <section className="flex items-center">
+                          <Image
+                            className="pr-1"
+                            alt="Profile Icon"
+                            src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${player.profileIconId}.jpg`}
+                            width={32}
+                            height={32}
+                            unoptimized
+                          />
+                          <div className="flex flex-col">
+                            <p className="font-semibold">
+                              {player.gameName}#{player.tagLine}
+                            </p>
+                            <div className="text-xs font-light">
+                              <div className="flex items-center gap-1">
+                                {isWithinDays(
+                                  player.lastUpdatedAt,
+                                  new Date(),
+                                  3,
+                                ) && (
+                                  <>
+                                    <Image
+                                      src={getRankEmblem(
+                                        player.rankTier as Tier,
+                                      )}
+                                      alt={player.rankTier}
+                                      quality={100}
+                                      width={16}
+                                      height={16}
+                                      unoptimized
+                                    />
+                                    <p>{player.lp} LP</p>
+                                    <Dot size={10} />
+                                  </>
+                                )}
+                                <p>LVL {player.summonerLevel}</p>
                               </div>
                             </div>
                           </div>
-                          {index == 0 && (
-                            <Kbd
-                              keys={"enter"}
-                              className="mr-1 rounded-md border border-zinc-300 p-1 text-center text-xs text-zinc-300"
-                            >
-                              Enter
-                            </Kbd>
-                          )}
-                        </div>
+                        </section>
+                        {index == 0 && (
+                          <Kbd
+                            keys={"enter"}
+                            className="rounded-md border border-zinc-300 p-1 text-center text-xs text-zinc-300"
+                          >
+                            Enter
+                          </Kbd>
+                        )}
                       </Link>
                     </li>
                   ))}
                 </ul>
               )}
-              {inputHasFocus && isPending && searchFilter.length > 0 && (
+              {isPending && searchFilter.length > 0 && (
                 <div className="flex items-center justify-center p-2">
                   <p className="font-semibold">Loading...</p>
                 </div>
               )}
-              {inputHasFocus &&
-                data &&
-                searchFilter.length > 0 &&
-                data.players.length === 0 && (
-                  <div className="flex items-center justify-center p-2">
-                    <div className="flex flex-col items-center justify-center text-center">
-                      <p className="font-semibold">No players found.</p>
-                      <p className="text-sm font-light">
-                        (Players without any dodges will not show up)
-                      </p>
-                    </div>
+              {data && searchFilter.length > 0 && data.players.length === 0 && (
+                <div className="flex items-center justify-center p-2">
+                  <div className="flex flex-col items-center justify-center text-center">
+                    <p className="font-semibold">No players found.</p>
+                    <p className="text-sm font-light">
+                      (Players without any dodges will not show up)
+                    </p>
                   </div>
-                )}
-              {inputHasFocus && error && searchFilter.length > 0 && (
+                </div>
+              )}
+              {error && searchFilter.length > 0 && (
                 <div className="flex items-center justify-center p-2">
                   <p>Search error: {error.message}</p>
                 </div>
