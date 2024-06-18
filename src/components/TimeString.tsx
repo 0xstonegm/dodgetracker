1b07a1e9306ac27e193074426ee7619fd3e8118f
Isak <isak.drevstad10@gmail.com>
2024-06-18 17:20:57 +0200

    fix(TimeString): Suppress hydration error

--- src/components/TimeString.tsx
+++ src/components/TimeString.tsx
@@ -3,13 +3,9 @@
 import { format } from "date-fns";
 import { useEffect, useState } from "react";
 import { timeDiffString } from "../lib/utils";
-import withNoSSR from "./higherOrder/withNoSSR";
 
 function TimeString({ utcTime: utcTime }: { utcTime: Date | null }) {
   const [, setTick] = useState(0); // Use state to force re-render
-
-  // console.log(utcTime.toLocaleString());
-  //
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -17,7 +13,7 @@ function TimeString({ utcTime: utcTime }: { utcTime: Date | null }) {
     }, 5000);
 
     return () => clearInterval(interval);
-  }, []); // Empty dependency array means this effect runs only once on mount
+  }, []);
 
   if (utcTime === null) {
     return <p>Time unknown</p>;
@@ -26,10 +22,10 @@ function TimeString({ utcTime: utcTime }: { utcTime: Date | null }) {
   const formattedDate = format(utcTime, "HH:mm:ss, eee do 'of' MMMM yyyy");
 
   return (
-    <p title={`Dodge detected at:\n${formattedDate}`}>
+    <p title={`Dodge detected at:\n${formattedDate}`} suppressHydrationWarning>
       {timeDiffString(utcTime)}
     </p>
   );
 }
 
-export default withNoSSR(TimeString);
+export default TimeString;
