33076fbb368ed9dd96e8a706df79b18455eccb46
Isak <isak.drevstad10@gmail.com>
2024-06-21 22:53:38 +0200

    feat(FetchButton): Show notif if button is pressed many times

--- src/components/FetchButton.tsx
+++ src/components/FetchButton.tsx
@@ -2,16 +2,21 @@
 
 import { sendGTMEvent } from "@next/third-parties/google";
 import { useLocalStorage, useVisibilityChange } from "@uidotdev/usehooks";
-import { Check, Loader2 } from "lucide-react";
+import { Check, Lightbulb, Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import posthog from "posthog-js";
 import { useCallback, useEffect, useRef, useState, useTransition } from "react";
+import toast from "react-hot-toast";
 import { autoFetchKey } from "../autoFetch";
 import { cn, secondsBetween } from "../lib/utils";
+import Toast from "./Toast";
 import withNoSSR from "./higherOrder/withNoSSR";
 import { Button } from "./ui/button";
 
 const updateIntervalSecs = 15;
+
+const notifPressCountIntervalSecs = 10;
+const notifPressCountThreshold = 3;
 
 export type FetchButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
@@ -24,6 +29,7 @@ function FetchButton({ className, ...props }: FetchButtonProps) {
   const tabVisible = useVisibilityChange();
   const interval = useRef<number | null>(null);
   const lastFetchTime = useRef<Date>(new Date());
+  const [presses, setPresses] = useState(new Set<Date>());
 
   // Fetch new dodges
   const fetch = useCallback(
@@ -101,6 +107,29 @@ function FetchButton({ className, ...props }: FetchButtonProps) {
     }
   }, [tabVisible, autoFetch, fetch, resetInterval, clearInterval]);
 
+  const showNotification = () => {
+    toast.custom(
+      (t) => (
+        <Toast className="flex flex-col" t={t}>
+          <div className="flex items-center gap-1">
+            <Lightbulb />
+            <p className="font-semibold">Dodge not appearing?</p>
+          </div>
+          <p className="text-sm font-light">
+            Dodges can take up to 30s to be tracked during heavy loads of the
+            League API. The player who dodged could also be below master tier
+            (or 0 LP), if so, the dodge can not be tracked.
+          </p>
+        </Toast>
+      ),
+      {
+        id: "many-presses-toast",
+        duration: 10 * 1000,
+      },
+    );
+    posthog.capture("many_fetches");
+  };
+
   return (
     <Button
       disabled={isPending || isDone}
@@ -112,6 +141,23 @@ function FetchButton({ className, ...props }: FetchButtonProps) {
       onClick={() => {
         fetch("fetch_clicked");
         resetInterval();
+
+        if (presses.size + 1 >= notifPressCountThreshold) {
+          showNotification();
+          setPresses(new Set());
+        } else {
+          const now = new Date();
+          setPresses((prev) => {
+            return new Set(prev).add(now);
+          });
+          setTimeout(() => {
+            setPresses((prev) => {
+              const newPresses = new Set(prev);
+              newPresses.delete(now);
+              return newPresses;
+            });
+          }, notifPressCountIntervalSecs * 1000);
+        }
       }}
       {...props}
     >
