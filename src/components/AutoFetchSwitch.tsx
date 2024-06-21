928e6eb61971a74e40db9b8e5aec7c90bdf89b0a
Isak <isak.drevstad10@gmail.com>
2024-06-21 21:01:27 +0200

    feat(AutoFetchSwitch): Show toast when changing auto fetch

--- src/components/AutoFetchSwitch.tsx
+++ src/components/AutoFetchSwitch.tsx
@@ -2,7 +2,10 @@
 
 import { useLocalStorage } from "@uidotdev/usehooks";
 import posthog from "posthog-js";
+import toast from "react-hot-toast";
 import { autoFetchKey } from "../autoFetch";
+import { cn } from "../lib/utils";
+import Toast from "./Toast";
 import withNoSSR from "./higherOrder/withNoSSR";
 import { Switch } from "./ui/switch";
 
@@ -10,6 +13,31 @@ function AutoFetchSwitch() {
   const [autoFetch, setAutoFetch] = useLocalStorage(autoFetchKey, false);
 
   const handleCheckedChange = (value: boolean) => {
+    toast.custom(
+      (t) => (
+        <Toast t={t}>
+          <p className="flex">
+            Auto-fetch{" "}
+            <p
+              className={cn("ml-1 rounded-md px-1 font-semibold", {
+                "bg-green-800": value,
+                "bg-red-800": !value,
+              })}
+            >
+              {value ? " enabled" : " disabled"}
+            </p>
+          </p>
+          {value && (
+            <p className="mt-1 text-sm font-light">
+              Dodges will automatically be fetched every 15 seconds.
+            </p>
+          )}
+        </Toast>
+      ),
+      {
+        id: "auto-fetch-toast",
+      },
+    );
     setAutoFetch(value);
     posthog.capture("auto_fetch_switch", {
       enabled: value,
