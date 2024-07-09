613e40560f43a044ffcd9cc2bc8b973ae03aacd9
Isak <isak.drevstad10@gmail.com>
2024-07-09 17:26:05 +0200

    fix(scripts): Don't hardcode node path

--- scripts/run_backend.sh
+++ scripts/run_backend.sh
@@ -14,12 +14,6 @@ done
 
 PROJECT_ROOT_DIR=$(realpath "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)/../")
 
-if [ "$HOSTNAME" = "HP" ]; then
-    NODE_BIN="/usr/bin/node"
-else
-    NODE_BIN="/home/isak102/.nvm/versions/node/v21.6.2/bin/node"
-fi
-
 cd "$PROJECT_ROOT_DIR" || exit
 
 source .env
@@ -38,7 +32,7 @@ echo "Build completed."
 if "$RUN"; then
     echo "Starting backend..."
     while true; do
-        "$NODE_BIN" src/backend/dist/backend/main.js
+        node src/backend/dist/backend/main.js
     done
     echo "Backend finished running."
 fi
