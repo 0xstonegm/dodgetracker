397f4dfb2f322d5edf17c2668e02c08e7d5388c8
Isak <isak.drevstad10@gmail.com>
2024-07-25 12:18:00 +0200

    refactor(leaderboard): Add LeaderboardEntry component

--- src/features/leaderboard/components/Leaderboard.tsx
+++ src/features/leaderboard/components/Leaderboard.tsx
@@ -1,15 +1,10 @@
 import { unstable_cache } from "next/cache";
 import { notFound } from "next/navigation";
 import PaginationControls from "../../../components/PaginationControls";
-import RankInfo from "../../../components/RankInfo";
 import { getLeaderboard } from "../../../data";
-import { type Tier } from "../../../lib/types";
-import { cn } from "../../../lib/utils";
 import { userRegionToRiotRegion } from "../../../regions";
 import { isCurrentSeason } from "../../../seasons";
-import { StatSite } from "../../../statSites";
-import SmallProfileCard from "../../player/components/SmallProfileCard";
-import StatSiteButton from "../../player/components/StatSiteButton";
+import LeaderboardEntry from "./LeaderboardEntry";
 
 const pageSize = 50;
 const maxPages = 100;
@@ -60,73 +55,15 @@ export default async function Leaderboard({
   return (
     <>
       {leaderboard.data.map((entry, index) => (
-        <div key={index}>
-          <div className="flex border-b border-zinc-900 p-2">
-            <div className="flex flex-grow flex-col">
-              <div
-                className={cn("grid gap-2", {
-                  "grid-cols-[0.15fr,2.5fr,0.7fr,0.7fr]": currentSeason,
-                  "grid-cols-[0.15fr,2.5fr,0.7fr]": !currentSeason,
-                })}
-              >
-                <p className="flex items-center justify-center font-bold md:text-lg">
-                  {(pageNumber - 1) * pageSize + index + 1}.
-                </p>
-                <section className="flex flex-wrap items-center md:text-xl">
-                  <SmallProfileCard
-                    gameName={entry.gameName}
-                    tagLine={entry.tagLine}
-                    profileIconId={entry.profileIconId}
-                    userRegion={userRegion}
-                    profileLink={true}
-                  />
-                  <div className="flex flex-wrap md:items-center md:justify-center">
-                    {entry.lolProsSlug && (
-                      <div className="mr-1">
-                        <StatSiteButton
-                          className="text-xs"
-                          riotRegion={entry.riotRegion}
-                          gameName={entry.gameName}
-                          tagLine={entry.tagLine}
-                          statSite={StatSite.LOLPROS}
-                          lolProsSlug={entry.lolProsSlug}
-                        />
-                      </div>
-                    )}
-                    <div>
-                      <StatSiteButton
-                        statSite={StatSite.OPGG}
-                        riotRegion={entry.riotRegion}
-                        gameName={entry.gameName}
-                        tagLine={entry.tagLine}
-                      />
-                    </div>
-                    <div className="pl-1">
-                      <StatSiteButton
-                        statSite={StatSite.DEEPLOL}
-                        riotRegion={entry.riotRegion}
-                        gameName={entry.gameName}
-                        tagLine={entry.tagLine}
-                      />
-                    </div>
-                  </div>
-                </section>
-                {currentSeason && (
-                  <RankInfo
-                    rankTier={entry.rankTier as Tier}
-                    lp={entry.currentLP}
-                  />
-                )}
-                <div className="flex justify-end">
-                  <div className="flex w-fit flex-col items-center justify-center">
-                    <p>{entry.numberOfDodges}</p>
-                    <p className="text-xs">dodges</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <LeaderboardEntry
+          key={index}
+          entry={entry}
+          index={index}
+          userRegion={userRegion}
+          pageNumber={pageNumber}
+          pageSize={pageSize}
+          includeRankInfo={currentSeason ?? false}
+        />
       ))}
       {totalPageCount > 1 && (
         <div className="flex justify-center pt-2">
---
+++ src/features/leaderboard/components/LeaderboardEntry.tsx
@@ -0,0 +1,81 @@
+import RankInfo from "@/src/components/RankInfo";
+import { type getLeaderboard } from "@/src/data";
+import { type Tier } from "@/src/lib/types";
+import { cn } from "@/src/lib/utils";
+import { StatSite } from "@/src/statSites";
+import SmallProfileCard from "../../player/components/SmallProfileCard";
+import StatSiteButton from "../../player/components/StatSiteButton";
+
+export default function LeaderboardEntry(props: {
+  entry: Awaited<ReturnType<typeof getLeaderboard>>["data"][number];
+  index: number;
+  userRegion: string;
+  pageNumber: number;
+  pageSize: number;
+  includeRankInfo: boolean;
+}) {
+  return (
+    <div
+      key={props.index}
+      className={cn("grid gap-2 border-b border-zinc-900 p-2", {
+        "grid-cols-[0.15fr,2.5fr,0.7fr,0.7fr]": props.includeRankInfo,
+        "grid-cols-[0.15fr,2.5fr,0.7fr]": !props.includeRankInfo,
+      })}
+    >
+      <p className="flex items-center justify-center font-bold md:text-lg">
+        {(props.pageNumber - 1) * props.pageSize + props.index + 1}.
+      </p>
+      <section className="flex flex-wrap items-center md:text-xl">
+        <SmallProfileCard
+          gameName={props.entry.gameName}
+          tagLine={props.entry.tagLine}
+          profileIconId={props.entry.profileIconId}
+          userRegion={props.userRegion}
+          profileLink={true}
+        />
+        <div className="flex flex-wrap md:items-center md:justify-center">
+          {props.entry.lolProsSlug && (
+            <div className="mr-1">
+              <StatSiteButton
+                className="text-xs"
+                riotRegion={props.entry.riotRegion}
+                gameName={props.entry.gameName}
+                tagLine={props.entry.tagLine}
+                statSite={StatSite.LOLPROS}
+                lolProsSlug={props.entry.lolProsSlug}
+              />
+            </div>
+          )}
+          <div>
+            <StatSiteButton
+              statSite={StatSite.OPGG}
+              riotRegion={props.entry.riotRegion}
+              gameName={props.entry.gameName}
+              tagLine={props.entry.tagLine}
+            />
+          </div>
+          <div className="pl-1">
+            <StatSiteButton
+              statSite={StatSite.DEEPLOL}
+              riotRegion={props.entry.riotRegion}
+              gameName={props.entry.gameName}
+              tagLine={props.entry.tagLine}
+            />
+          </div>
+        </div>
+      </section>
+      {props.includeRankInfo && (
+        <RankInfo
+          rankTier={props.entry.rankTier as Tier}
+          lp={props.entry.currentLP}
+        />
+      )}
+      <div className="flex justify-end">
+        <div className="flex w-fit flex-col items-center justify-center">
+          <p>{props.entry.numberOfDodges}</p>
+          <p className="text-xs">dodges</p>
+        </div>
+      </div>
+    </div>
+  );
+}
